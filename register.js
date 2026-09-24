a4588d8f2159887aa2cec1e2ccc450749767af46
karacbKIRILL <kirillgulaev065@gmail.com>
2026-09-24 12:58:16 +0500

    Fix import path for firebase-config.js

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase-config.js";
+import { auth, db } from "firebase-config.js";
 import { createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
